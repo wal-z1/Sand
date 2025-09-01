23271d1bb3dbe76f0b23d57adbea96bd765cb237
Walid <ow_bouhenika@esi.dz>
2025-09-01 15:52:42 +0100

    sho

--- src/Comp/Header/Navbar.jsx
+++ src/Comp/Header/Navbar.jsx
@@ -1,13 +1,13 @@
 export default function NavBar() {
 	return (
-		<nav className="flex gap-6 flex-wrap justify-start">
-			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition">
+		<nav className="flex gap-6 flex-wrap justify-start ">
+			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition cursor-pointer">
 				Button 1
 			</button>
-			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition">
+			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition cursor-pointer">
 				Button 2
 			</button>
-			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition">
+			<button className="text-[#EAEAEA] hover:text-[#E5C07B] transition cursor-pointer">
 				Button 3
 			</button>
 		</nav>
