6efcbdf412107a961322677a040353aec7e30288
Walid <ow_bouhenika@esi.dz>
2025-09-01 21:26:52 +0100

    animations are lest step of design ok dont get me started with those

--- src/Comp/Pages/Home.jsx
+++ src/Comp/Pages/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-	return <div>Home</div>;
+	return (
+		<>
+			{/*we are deviding the website into 3 secitions left and righ and middle
+					a grid is better 
+			*/}
+			<div className=""></div>
+		</>
+	);
 }
 
 export default Home;
