2f785162a8ffe087325bbdcc9aa32e88e7c1234a
Walid <ow_bouhenika@esi.dz>
2025-09-01 14:03:53 +0100

    yeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+
 
 function App() {
-	const [count, setCount] = useState(0);
+
 
 	return (
 		<>
-		
+
 			<h1 class="text-3xl font-bold underline font-sora text-white">
 				Hello world!
 			</h1>
