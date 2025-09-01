82aef2448d5556dbcff2984bf0ebcc75c0ab7c81
Walid <ow_bouhenika@esi.dz>
2025-09-01 14:06:27 +0100

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-
+import { Analytics } from "@vercel/analytics/react";
+import { Routes, Route } from "react-router-dom";
 function App() {
-
-
 	return (
 		<>
-
-			<h1 class="text-3xl font-bold underline font-sora text-white">
-				Hello world!
-			</h1>
+			<Routes>
+				<Route path="step-2" element={<></>}></Route>
+				<h1 class="text-3xl font-bold underline font-sora text-white">
+					Hello world!
+				</h1>
+			</Routes>
 		</>
 	);
 }
