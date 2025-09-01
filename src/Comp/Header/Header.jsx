2016d2ef78e64b4009888a6f5b7ce46560637cb4
Walid <ow_bouhenika@esi.dz>
2025-09-01 15:51:38 +0100

    sticky

--- src/Comp/Header/Header.jsx
+++ src/Comp/Header/Header.jsx
@@ -3,7 +3,7 @@ import HeaderButtons from "./HeaderButtons";
 
 export default function Header() {
 	return (
-		<header className="bg-[#0F0F0F] border-b border-[hsl(0,0%,20%)] font-source-sans font-bold">
+		<header className="sticky top-0 bg-[#0f0f0f71] border-b border-[hsl(0,0%,20%)] font-source-sans font-bold backdrop-blur-md">
 			<div className="mx-auto flex h-16 max-w-screen-xl items-center justify-between px-4 sm:px-6 lg:px-8">
 				{/*div to center the buttons and logo*/}
 				<div className="flex items-center gap-8">
