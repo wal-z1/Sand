0351cb1a8adbba25baadd189602aa2f97fe48141
Walid <ow_bouhenika@esi.dz>
2025-09-02 18:36:17 +0100

    later TODO AND OTHER SECTIONS

--- src/Comp/Sections/MainSection.jsx
+++ src/Comp/Sections/MainSection.jsx
@@ -1,7 +1,19 @@
-
-
 function MainSection() {
-	return <div>MainSection</div>;
+	return (
+		<div>
+			<h3>Exploring the New JavaScript Features</h3>
+			<p>A quick overview of ES2025 updates…</p>
+			<p>
+				<em>by Walid, 2h ago</em>
+			</p>
+			<p>👍 23 | 👎 2 | 💬 5</p>
+			 <h3>Why Web Performance Matters</h3>
+    <p>Tips to optimize load times…</p>
+    <p><em>by Houssam, 5h ago</em></p>
+    <p>👍 41 | 👎 1 | 💬 12</p>
+		
+		</div>
+	);
 }
 
 export default MainSection;
--- src/Comp/Sections/RightSide.jsx
+++ src/Comp/Sections/RightSide.jsx
@@ -1,5 +1,16 @@
 function RightSide() {
-	return <div>RightSide</div>;
+	return (
+		<div>
+			{" "}
+			<h4>Online Contacts</h4>
+			<ul>
+				<li>Alice – Online</li>
+				<li>Bob – Online</li>
+				<li>Charlie – Away</li>
+				<li>Diana – Online</li>
+			</ul>
+		</div>
+	);
 }
 
 export default RightSide;
