6dd82fe510090af6ac2019417fe788cac08488c4
Walid <ow_bouhenika@esi.dz>
2025-09-05 19:28:39 +0100

    rework container

--- src/Comp/Ui/AllPostContainer.jsx
+++ src/Comp/Ui/AllPostContainer.jsx
@@ -1,6 +1,12 @@
 function AllPostContainer({ children }) {
 	return (
-		<div className="w-full mx-auto flex flex-col gap-6 p-3 sm:p-4 bg-[#0F0F0F] border border-[#2A2A2A] rounded-xl">
+		<div
+			className="
+				mx-auto flex flex-col gap-6 
+				p-2 sm:p-3 
+				bg-[#0F0F0F] border border-[#2A2A2A] rounded-xl 
+				w-full max-w-[900px] lg:max-w-[1000px]
+			">
 			{children}
 		</div>
 	);
