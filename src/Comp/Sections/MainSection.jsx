cf8e87b30ddb77909a49bd108b6fc2d308aea9b5
Walid <ow_bouhenika@esi.dz>
2025-09-06 11:58:15 +0100

    sort button works

--- src/Comp/Sections/MainSection.jsx
+++ src/Comp/Sections/MainSection.jsx
@@ -2,21 +2,43 @@ import SortButtonsGroups from "../Ui/SortButtonsGroups";
 import PostCard from "../Ui/PostCard";
 import dummyPosts from "../../lib/dummyPostData";
 import AllPostContainer from "../Ui/AllPostContainer";
+import { useState } from "react";
+
 function MainSection() {
+	const [sortedArray, updatesorted] = useState(
+		[...dummyPosts].sort(
+			(a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+		) // default newest first
+	);
+
 	return (
 		<div className="mt-1.5">
-			<SortButtonsGroups //what to do with the value sekected
-				onSelect={(val) => console.log("Selected:", val)}
+			<SortButtonsGroups
+				onSelect={(val) => {
+					console.log("Selected:", val);
+					updatesorted((prev) => {
+						if (val === "old") {
+							return [...prev].sort(
+								(a, b) => new Date(a.timestamp) - new Date(b.timestamp)
+							);
+						} else if (val === "new") {
+							return [...prev].sort(
+								(a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+							);
+						} else {
+							return [...prev].sort((a, b) => b.ups - a.ups); // top first
+						}
+					});
+				}}
 			/>
-			<span className=" flex items-center">
+			<span className="flex items-center">
 				<span className="shrink-0 pe-4 text-[#EAEAEA] font-sora">Feed</span>
 				<span className="h-[1px] flex-1 bg-gradient-to-l from-[#e1e4cc] to-[#C2B280]"></span>
 			</span>
 			<div>
 				<AllPostContainer>
-					{/*a post*/}
-					{dummyPosts.map((p) => (
-						<PostCard post={p} />
+					{sortedArray.map((p) => (
+						<PostCard key={p.id} post={p} />
 					))}
 				</AllPostContainer>
 			</div>
