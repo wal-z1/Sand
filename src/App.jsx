08b373a7f1a927db0a9bb37258375ca0d2523ee8
Walid <ow_bouhenika@esi.dz>
2025-09-06 12:52:42 +0100

    scroll up on path change

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { ViewProvider } from "./Context/ViewContext";
 import Header from "./Comp/Header/Header";
 import Home from "./Comp/Pages/Home";
 import { useEffect } from "react";
 
-function App() {
+function ScrollToTop() {
+	const { pathname } = useLocation();
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
-	}, []);
-	// reset the scroll position
+	}, [pathname]); // runs when changing route
+
+	return null;
+}
+
+function App() {
 	return (
-		<>
-			<ViewProvider>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</ViewProvider>
-		</>
+		<ViewProvider>
+			<Header />
+			<ScrollToTop /> {/* needs to be included to run */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</ViewProvider>
 	);
 }
 
