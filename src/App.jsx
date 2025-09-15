ba8fe33119392026b3f28cc91335a7caa9fc67dc
Walid <ow_bouhenika@esi.dz>
2025-09-15 14:36:48 +0100

    back

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import Header from "./Comp/Header/Header";
 import Home from "./Comp/Pages/Home";
 import { useEffect } from "react";
 import About from "./Comp/Pages/About";
+
 function App() {
+	useEffect(() => {
+		async function TheBAckendFetchFUn() {
+			try {
+				const back = await fetch("/api");
+
+				console.log(back);
+			} catch (err) {
+				console.error("error from backend", err);
+			}
+		}
+		TheBAckendFetchFUn();
+	}, []);
+
 	return (
 		<ViewProvider>
 			<Header />
-
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
