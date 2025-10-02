d4eba181fd78f81d05f61070c0d6b25aa1c7b9b2
Walid <ow_bouhenika@esi.dz>
2025-10-02 13:51:14 +0100

    added the left side bar into the about page

--- src/Comp/Pages/About.jsx
+++ src/Comp/Pages/About.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import LeftSide from "../Sections/LeftSide/LeftSide";
+import { UseViewContext } from "../../Context/ViewContext";
 
 // faq content data
 const FAQ_ITEMS = [
@@ -97,17 +99,34 @@ const FAQItem = ({ title, content, index }) => (
 );
 
 function About() {
+	const { LeftIsHidden } = UseViewContext();
+
 	return (
-		<div className="space-y-6 max-w-2xl mx-auto mt-16">
-			{FAQ_ITEMS.map((item, index) => (
-				<FAQItem
-					key={index}
-					title={item.title}
-					content={item.content}
-					index={index}
-				/>
-			))}
-		</div>
+		<motion.div
+			className="font-lexend sm:grid gap-3 text-[#EAEAEA]"
+			initial={{ opacity: 0.97 }}
+			animate={{
+				opacity: 1,
+				// adjust grid columns based on sidebar state
+				gridTemplateColumns: LeftIsHidden
+					? "65px 3fr 250px"
+					: "250px 3fr 250px",
+			}}
+			// smooth animation
+			transition={{ duration: 0.44, ease: "easeInOut" }}>
+			<LeftSide />
+
+			<div className="space-y-6 max-w-2xl mx-auto mt-16">
+				{FAQ_ITEMS.map((item, index) => (
+					<FAQItem
+						key={index}
+						title={item.title}
+						content={item.content}
+						index={index}
+					/>
+				))}
+			</div>
+		</motion.div>
 	);
 }
 
